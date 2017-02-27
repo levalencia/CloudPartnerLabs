c78112cfa9b0ff73e6f252bb6479a386ea60ee0c
Luis Valencia <le.valencia@outlook.com>
2017-02-26 19:54:05 -0500

    more demos

---
+++ DemoWebApp/DemoWebJob/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejecutado a: " + DateTime.Now.ToShortTimeString());
+        }
+    }
+}
